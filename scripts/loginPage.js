b9b87f6587a2228f1e15c3144ea79db92d767988
Tevel <misstevel@gmail.com>
2025-04-22 20:38:47 +0300

    change login +register pages

--- scripts/loginPage.js
+++ scripts/loginPage.js
@@ -5,4 +5,35 @@ $("#loginBtn").click((event)=>
     const email = $("#email").val();
     const password = $("#password").val();
 
+    if(!email || !password)
+    {
+        alert("Please enter Email and Password");
+        return;
+    }
+    
+    let users = JSON.parse(localStorage.getItem("Users"));
+    if(users)
+    {
+        for(let i in users)
+        {
+            let user= users[i];
+            if(user.email == email)
+            {
+                if(user.password == password)
+                {
+                    localStorage.setItem("currentUser", JSON.stringify(user));
+                    window.location.href="home.html";
+                    return;
+                }
+                else
+                {
+                    alert("Invalid password");
+                    return;
+                }
+            }
+        }
+        alert("No User found");
+    }
+
+
 })
